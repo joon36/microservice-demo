719842f4743cb6065fbefd6170835644908d9c27
joon36 <kwjoon7@gmail.com>
2021-10-20 22:53:30 +0900

    world service 호출시 50% 확률로 3초 지연이 발생하도록 수정

--- world/src/main/java/com/example/world/WorldController.java
+++ world/src/main/java/com/example/world/WorldController.java
@@ -1,5 +1,6 @@
 package com.example.world;
 
+import java.util.Random;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,22 @@ public class WorldController {
 
     @GetMapping("")
     public String world() {
+        randomRunLong();
         return "World! #1";
+    }
+
+    private void randomRunLong() {
+        Random rand = new Random();
+        if (rand.nextInt(2) == 0) { // 50% 확률
+            sleep();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(3000); // 3초 지연
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
